54a8394481baf1b3f1f79645660fd69d881b61d0
HAB-Coder <habibamursaleen20@gmail.com>
2025-03-11 01:12:58 +0500

    add type animation

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ function toggleMenu() {
     navLinks.classList.toggle("active");
 }
 
+var typed = new Typed('#typed', {
+    strings: ["Frontened developer", "UI/UX Designer", "Full Stack Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
